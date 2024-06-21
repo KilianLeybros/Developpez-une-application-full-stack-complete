e3c8adafe9060bb84f5d40b1f3e2acad476f12bd
Kilian <k.leybros.15@gmail.com>
2024-06-21 17:11:34 +0200

    Creating CustomUserDetails and CustomUserDetailsService

---
+++ back/src/main/java/com/openclassrooms/mddapi/security/model/CustomUserDetails.java
@@ -0,0 +1,40 @@
+package com.openclassrooms.mddapi.security.model;
+
+import com.openclassrooms.mddapi.data.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class CustomUserDetails extends User implements UserDetails {
+
+    public CustomUserDetails(User user){
+        this.setEmail(user.getEmail());
+        this.setPassword(user.getPassword());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
