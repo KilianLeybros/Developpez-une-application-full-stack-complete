d032b806f6aa29d92cb200244879cb1dfed42b2b
Kilian <k.leybros.15@gmail.com>
2024-06-28 12:33:04 +0200

    Exception handling

---
+++ back/src/main/java/com/openclassrooms/mddapi/controller/handler/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.openclassrooms.mddapi.controller.handler;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> requestBodyNotValidHandler(MethodArgumentNotValidException exception){
+        Map<String, String> errorResult = new HashMap<String, String>();
+        exception.getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errorResult.put(fieldName, errorMessage);
+        });
+        return errorResult;
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String entityNotFoundHandler(EntityNotFoundException exception){
+        return exception.getMessage();
+    }
+}
