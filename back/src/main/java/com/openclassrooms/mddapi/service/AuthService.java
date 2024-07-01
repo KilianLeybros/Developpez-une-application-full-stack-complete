09fceff570aef5e8f0db38641462e2402e98a73b
Kilian <k.leybros.15@gmail.com>
2024-07-01 07:57:28 +0200

    same-site porperty

--- back/src/main/java/com/openclassrooms/mddapi/service/AuthService.java
+++ back/src/main/java/com/openclassrooms/mddapi/service/AuthService.java
@@ -67,6 +67,7 @@ public class AuthService {
             ResponseCookie cookie = ResponseCookie.from("token", token)
                     .httpOnly(true)
                     .secure(false)
+                    .sameSite("Lax")
                     .path("/")
                     .maxAge(cookieExpiration)
                     .build();
