7f84cc140c58347601ddac8b159f21d76d10a744
Kilian <k.leybros.15@gmail.com>
2024-07-06 18:29:41 +0200

    Implementing Controller endpoint

--- back/src/main/java/com/openclassrooms/mddapi/controller/TopicController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/TopicController.java
@@ -1,21 +1,39 @@
 package com.openclassrooms.mddapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.openclassrooms.mddapi.data.dto.topic.SubscribedTopic;
+import com.openclassrooms.mddapi.data.dto.topic.TopicResponse;
+import com.openclassrooms.mddapi.service.ITopicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/topic")
 public class TopicController {
 
+
+    @Autowired
+    private ITopicService topicService;
     @GetMapping
-    public String getAllTopic(){
-        return "Ok";
+    public ResponseEntity<List<TopicResponse>> getAllTopics(){
+        return ResponseEntity.ok(topicService.findAllTopics());
     }
 
-    @GetMapping
-    public String getSubscribed(){
-        return "OK";
+    @GetMapping("subscribed")
+    public ResponseEntity<List<SubscribedTopic>> getSubscribed(){
+        return ResponseEntity.ok(topicService.findSubscribedTopics());
+    }
+
+    @PostMapping("{id}/subscribe")
+    public ResponseEntity<TopicResponse> subscribe(@PathVariable(value = "id") Long id){
+        return ResponseEntity.ok(topicService.subscribe(id));
+    }
+
+    @PostMapping("{id}/unsubscribe")
+    public ResponseEntity<TopicResponse> unsubscribe(@PathVariable(value = "id") Long id){
+        return ResponseEntity.ok(topicService.unsubscribe(id));
     }
 
 }
