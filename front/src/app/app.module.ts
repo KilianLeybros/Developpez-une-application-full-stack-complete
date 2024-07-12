885343abe69ad0df7e45d0620e16d4ab2316a5aa
Kilian <k.leybros.15@gmail.com>
2024-07-12 16:51:49 +0200

    title for auth page

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -8,10 +8,25 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { AuthTitleComponent } from './pages/auth/auth-title/auth-title.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FakeHeaderComponent } from './shared/header/fake-header/fake-header.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, LoginComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, MatIconModule],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    HeaderComponent,
+    FakeHeaderComponent,
+    AuthTitleComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatIconModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
