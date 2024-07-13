fb566f1ef51617e371b21c2ea3a2978daa06cdb8
Kilian <k.leybros.15@gmail.com>
2024-07-13 13:46:36 +0200

    Creation of loggedin guard

--- front/src/app/guards/data-user.guard.ts
+++ front/src/app/guards/data-user.guard.ts
@@ -1,7 +1,7 @@
 import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { AuthService } from '../service/auth.service';
-import { catchError, first, map, of, switchMap, tap } from 'rxjs';
+import { first, map, of, switchMap } from 'rxjs';
 import { User } from '../interfaces/user.interface';
 
 export const dataUserGuard: CanActivateFn = () => {
@@ -11,11 +11,8 @@ export const dataUserGuard: CanActivateFn = () => {
     switchMap((user: User | null) => {
       if (user) return of(true);
       else {
-        return authService.fetchCurrentUser().pipe(
-          map(() => true)
-        );
+        return authService.fetchCurrentUser().pipe(map(() => true));
       }
-    }),
-    
+    })
   );
 };
