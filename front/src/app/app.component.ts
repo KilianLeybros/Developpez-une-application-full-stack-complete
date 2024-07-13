13f8aac3ab518f0c9e030d55374d0e5854d3c684
Kilian <k.leybros.15@gmail.com>
2024-07-13 13:47:43 +0200

    Reafacto app component

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,26 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './service/auth.service';
 import { first, Observable } from 'rxjs';
-import { User } from './interfaces/user.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  private isLoggedin$: Observable<boolean | null> =
+export class AppComponent {
+  public isLoggedin$: Observable<boolean> =
     this.authService.isLoggedin$.asObservable();
 
-  public isLoggedin: boolean | null = null;
-
   constructor(private authService: AuthService) {}
-
-  ngOnInit(): void {
-    this.authService.isLoggedin$
-      .pipe(first())
-      .subscribe((value: boolean | null) => {
-        this.isLoggedin = value;
-      });
-  }
 }
