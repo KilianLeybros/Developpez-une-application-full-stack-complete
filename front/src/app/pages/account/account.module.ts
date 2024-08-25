323929644c78bd9bd496df6ebd0bfe6acc39aa90
Kilian <k.leybros.15@gmail.com>
2024-08-26 00:22:08 +0200

    Refactoring topics

--- front/src/app/pages/account/account.module.ts
+++ front/src/app/pages/account/account.module.ts
@@ -4,9 +4,18 @@ import { AccountFormComponent } from './account-form/account-form.component';
 import { TopicModule } from '../topic/topic.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { PasswordFormComponent } from './password-form/password-form.component';
+import { MatIconModule } from '@angular/material/icon';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 @NgModule({
-  declarations: [AccountComponent, AccountFormComponent],
-  imports: [SharedModule, TopicModule, ReactiveFormsModule],
+  declarations: [AccountComponent, AccountFormComponent, PasswordFormComponent],
+  imports: [
+    SharedModule,
+    TopicModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    AppRoutingModule,
+  ],
 })
 export class AccountModule {}
